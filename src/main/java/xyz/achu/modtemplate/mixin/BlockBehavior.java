5c2c181025cbef867a4679d78fb227305556d164
Alexandria <alxpettit@gmail.com>
2022-01-12 04:15:50 -0800

    basic behavior working

--- src/main/java/xyz/achu/modtemplate/mixin/BlockBehavior.java
+++ src/main/java/xyz/achu/modtemplate/mixin/BlockBehavior.java
@@ -2,8 +2,11 @@ package xyz.achu.modtemplate.mixin;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EnderChestBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,8 +18,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockBehaviour.class)
 public abstract class BlockBehavior {
+    @Shadow protected abstract Block asBlock();
+
+    @Shadow public abstract Item asItem();
+
     @Inject(method = "attack", at = @At("HEAD"))
     public void attack(BlockState blockState, Level level, BlockPos blockPos, Player player, CallbackInfo ci) {
-        level.destroyBlock(blockPos, true);
+        if (this.asBlock() instanceof EnderChestBlock) {
+            ItemStack itemStack = new ItemStack(asItem());
+            boolean addedChestToInventory = player.getInventory().add(itemStack);
+            if (addedChestToInventory) {
+                level.removeBlock(blockPos, true);
+            }
+        }
     }
 }
